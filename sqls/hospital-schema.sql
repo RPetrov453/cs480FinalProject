31a9032a57d05204ae916962318916d0f7b005df
Robert Petrov <unaverage453@gmail.com>
2023-04-26 17:24:23 -0500

    added outline to schema, more to ERD

---
+++ sqls/hospital-schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS hospital;
+CREATE DATABASE hospital;
+USE hospital;
+
+create table patient(
+	pID int
+    
+);
+create table nurse(
+	
+    
+);
+create table physician(
+	
+    
+);
+create table room(
+	
+    
+);
+create table payment(
+	
+    
+);
+create table invoice(
+    
+);
+create table instruction(
+
+    
+);
+

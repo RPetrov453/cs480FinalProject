dee26f0ed0bb76a6aca33259dead146b68423237
Robert Petrov <unaverage453@gmail.com>
2023-05-01 12:33:25 -0500

    Added template for most of the files

---
+++ sqls/hospital-query.sql
@@ -0,0 +1,43 @@
+select * from;
+
+select * from;
+
+select * from;
+
+select * from;
+
+select * from;
+
+select * from;
+
+-- Three join queries -- 
+select * from patient;
+
+select * from;
+
+select * from;
+
+-- Three Aggregation Queries --
+select * from;
+
+select * from;
+
+select * from;
+
+-- Three Nested Queries
+select * from;
+
+select * from;
+
+select * from;
+
+
+-- 3 Views 
+
+
+-- 3 Triggers
+
+
+-- 2 Transactions
+
+
--- sqls/hospital-schema.sql
+++ sqls/hospital-schema.sql
@@ -3,13 +3,18 @@ CREATE DATABASE hospital;
 USE hospital;
 
 create table patient(
-	pID int,
-    
+	pID int Primary key,
+    phone int,
+    name varchar (50),
+    address varchar (100)
     
 );
 create table nurse(
-	
-    
+	nID int PRIMARY KEY,
+    nname varchar(50),
+    phone int,
+    cert int,
+    address varchar (100)
 );
 create table physician(
 	physID int PRIMARY KEY,
@@ -20,8 +25,9 @@ create table physician(
     address varchar (100)
 );
 create table room(
-	
-    
+	roomNum int primary key,
+    capacity int,
+    fee int
 );
 
 create table invoice(
@@ -31,5 +37,13 @@ create table instruction(
 	iCode int primary key,
     fee int,
     Descript varchar(100)
+);
+
+create table health_record(
+	rID int primary key,
+    descript varchar(100),
+    status varchar(50),
+    
+
 );
 

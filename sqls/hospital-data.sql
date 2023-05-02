3aa6e74ab72b85bde2e7b0d75860db458ce6a2a2
CodyAAutry <codyautryhd@gmail.com>
2023-05-01 19:45:45 -0500

    insertData

---
+++ sqls/hospital-data.sql
@@ -0,0 +1,49 @@
+-- HOSPITAL PATIENTS --
+insert into patient (pID, phone, name, address)
+values (124, '1234567890', 'John Jones', '12345 West Meadow Lane');
+
+insert into patient (pID, phone, name, address)
+values (635, '2345678901', 'Sabrina Osborne', '7638 Hamilton Circle');
+
+insert into patient (pID, phone, name, address)
+values (785, '3456789012', 'Vera Sparks', '629 Pineknoll Drive');
+
+insert into patient (pID, phone, name, address)
+values (789, '4567890123', 'Michael Crawford', '447 Arrowhead Court');
+
+insert into patient (pID, phone, name, address)
+values (456, '5678901234', 'Marion Atkins', '139 Alderwood Street');
+
+
+-- HOSPITAL NURSES --
+insert into nurse (nID, nname, phone, cert, address)
+values (786,'Robert Petrov', '1325839451', '3152', '1204 North Kingston Ave');
+
+insert into nurse (nID, nname, phone, cert, address)
+values (486,'robert petrov', '5667564681', '6214', '401 West Maple Road');
+
+insert into nurse (nID, nname, phone, cert, address)
+values (453, 'robert petrov', '9843156481', '1354', '222 Hudson Ave');
+
+insert into nurse (nID, nname, phone, cert, address)
+values (687, 'robert petrov', '6451346842', '4562', '68 East Yukon Street');
+
+insert into nurse (nID, nname, phone, cert, address)
+values (157, 'robert petrov', '9456215798', '6542', '66 Glenwood Road');
+
+
+-- HOSPITAL PHYSICIANS -- 
+insert into physician (physID, pname, phone, cert, address)
+values (712, 'Cody Autry', '4561567894', '9999', '5896 East SunnySide Grove');
+
+insert into physician (physID, pname, phone, cert, address)
+values (345, 'Alexander Fleming', '6483154687', '9998', '9874 South Laneston Street');
+
+insert into physician (physID, pname, phone, cert, address)
+values (978, 'Cody Autry', '9543214596', '9997', '9 Wild Horse Ave');
+
+insert into physician (physID, pname, phone, cert, address)
+values (841, 'Cody Autry', '7486543215', '9996', '7427 Glen Eagles Ave');
+
+insert into physician (physID, pname, phone, cert, address)
+values (642, 'Cody Autry', '7851354687', '9995', '73 East Augusta Road');
